33bbc7fdd70a70a52e1e11d34ca3d434638778dc
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-17 00:18:31 +0530

    String reverse without using String inbuilt functions or methods.

---
+++ src/CodingQuestions/StringReverseWithoutBuilder.java
@@ -0,0 +1,25 @@
+package CodingQuestions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+class StringReverseWithoutBuilder
+{
+
+    public static String stringReverse(String word)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : word.toCharArray())
+        {
+            stack.push(c);
+        }
+        String res = stack.stream().map(String::valueOf).collect(Collectors.joining());
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(StringReverseWithoutBuilder.stringReverse("virtusa"));
+    }
+}
