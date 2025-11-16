65117e8a6ccc7f4b6a27beebae0986c1aff7570b
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-17 00:36:58 +0530

    swap numbers with and without using third variable.

---
+++ src/CodingQuestions/SwapTwoNumbers.java
@@ -0,0 +1,28 @@
+package CodingQuestions;
+
+public class SwapTwoNumbers
+{
+    public static void swapNumbers1(int a, int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a : "+a+" b : "+b);
+    }
+
+    public static void swapNumbers2(int a, int b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a : "+a+" b : "+b);
+    }
+
+    public static void main(String[] args) {
+        SwapTwoNumbers.swapNumbers1(10, 15);
+        SwapTwoNumbers.swapNumbers2(10, 15);
+    }
+
+
+}
