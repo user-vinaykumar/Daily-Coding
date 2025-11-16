55286082d696f84f46283723c72f66a376a10932
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-16 22:54:47 +0530

    prime Number checker, programming.

---
+++ src/CodingQuestions/ProbablePrime.java
@@ -0,0 +1,20 @@
+package CodingQuestions;
+
+import java.math.BigInteger;
+
+public class ProbablePrime
+{
+
+    public static boolean isPrime(int number)
+    {
+        return BigInteger.valueOf(number).isProbablePrime(number);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(ProbablePrime.isPrime(4));
+        System.out.println(ProbablePrime.isPrime(3));
+    }
+
+
+}
