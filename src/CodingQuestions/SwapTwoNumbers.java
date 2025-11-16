8a0681310d3ba97f7e412aa4046744a902778ce9
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-17 00:38:44 +0530

    unused lines are removed.

--- src/CodingQuestions/SwapTwoNumbers.java
+++ src/CodingQuestions/SwapTwoNumbers.java
@@ -1,22 +1,19 @@
 package CodingQuestions;
 
-public class SwapTwoNumbers
-{
-    public static void swapNumbers1(int a, int b)
-    {
+public class SwapTwoNumbers {
+    public static void swapNumbers1(int a, int b) {
         int temp = a;
         a = b;
         b = temp;
 
-        System.out.println("a : "+a+" b : "+b);
+        System.out.println("a : " + a + " b : " + b);
     }
 
-    public static void swapNumbers2(int a, int b)
-    {
+    public static void swapNumbers2(int a, int b) {
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println("a : "+a+" b : "+b);
+        System.out.println("a : " + a + " b : " + b);
     }
 
     public static void main(String[] args) {
