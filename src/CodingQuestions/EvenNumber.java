1d7f18cff7433832bd12a1e965df7eeb9c4c1728
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-16 22:59:34 +0530

    Even Number in a List of Integers.

---
+++ src/CodingQuestions/EvenNumber.java
@@ -0,0 +1,23 @@
+package CodingQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenNumber
+{
+
+    public static List<Integer> evenNumber(List<Integer> list)
+    {
+        return list.stream().filter(n->n%2==0).toList();
+    }
+
+
+
+    public static void main(String[] args) {
+        List<Integer> inputLIst = Arrays.asList(2, 3, 4, 5, 6, 7, 89, 22, 11, 34);
+        System.out.println(EvenNumber.evenNumber(inputLIst));
+    }
+
+
+}
