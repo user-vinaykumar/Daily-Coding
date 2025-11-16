d1b775c546adbe780f6798c10e2c169dd6f20d43
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-16 22:37:36 +0530

    Palindrome checker.

---
+++ src/CodingQuestions/Palindrome.java
@@ -0,0 +1,17 @@
+package CodingQuestions;
+
+public class Palindrome
+{
+    public static boolean isPalindrome(String word)
+    {
+        String term = String.valueOf(word);
+        return term.equals(new StringBuilder(term).reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("malayalam"));
+        System.out.println(Palindrome.isPalindrome("virtusa"));
+    }
+
+
+}
