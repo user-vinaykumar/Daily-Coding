ca696408bb0d8fd33b7296f1d5dfe443cdcdbbc8
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-16 23:36:36 +0530

    character in a string that has repeated more number of times.

---
+++ src/CodingQuestions/HighCharCount.java
@@ -0,0 +1,30 @@
+package CodingQuestions;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HighCharCount
+{
+    public static void highCharCount(String word)
+    {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for(char c : word.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        Map.Entry<Character, Integer> maxEntry = map.entrySet().stream()
+                .max(Map.Entry.comparingByValue()).orElse(null);
+        if(maxEntry!= null)
+        {
+            System.out.println(maxEntry.getKey()+":"+maxEntry.getValue());
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        HighCharCount.highCharCount("banana");
+
+    }
+}
