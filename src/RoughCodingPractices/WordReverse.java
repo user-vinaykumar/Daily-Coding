d977344f3ab412583adedc7735485989e6a03412
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-18 14:28:20 +0530

    string reverse code - wiht string builder class.

---
+++ src/RoughCodingPractices/WordReverse.java
@@ -0,0 +1,12 @@
+package RoughCodingPractices;
+
+public class WordReverse {
+    public static String wordReverse(String word) {
+        return new StringBuilder(word).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(WordReverse.wordReverse("virtusa"));
+    }
+
+}
