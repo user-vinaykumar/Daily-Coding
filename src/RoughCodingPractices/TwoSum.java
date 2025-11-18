b3c1fae90f10edaf35877bc0d0ed7ac347b9b80e
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-18 14:39:13 +0530

    two sum algo program.

---
+++ src/RoughCodingPractices/TwoSum.java
@@ -0,0 +1,30 @@
+package RoughCodingPractices;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum
+{
+    public static int[] twoSum(int[]nums, int target)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++)
+        {
+            int difference = target - nums[i];
+            if(map.containsKey(difference))
+            {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {2, 3, 4, 5, 7, 9};
+        int[]result = TwoSum.twoSum(nums, 6);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
