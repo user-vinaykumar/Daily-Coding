ad9bc614925ef6b57686694dc0a32e42df5fdbb6
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-18 14:35:15 +0530

    character count of the string

---
+++ src/RoughCodingPractices/WordCharCount.java
@@ -0,0 +1,27 @@
+package RoughCodingPractices;
+
+import CodingQuestions.WordReverse;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WordCharCount
+{
+    public static void wordCharCount(String word)
+    {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for(char c : word.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        for(Map.Entry<Character, Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        WordCharCount.wordCharCount("virtusa");
+    }
+
+}
