22d9b2c874b72e3ca98d0a2ec451927141320894
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-21 11:51:46 +0530

    word reverse with string builder function.

---
+++ src/PracticeDay03/WordReverse.java
@@ -0,0 +1,13 @@
+package PracticeDay03;
+
+public class WordReverse {
+
+    public static String wordReverse(String word)
+    {
+        return new StringBuilder(word).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(WordReverse.wordReverse("virtusa"));
+    }
+}
