065aafb8c7724f9f1f75a88d92b6f61127545318
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-21 12:07:39 +0530

    program to check the fibonacci series of the given input number of times.

---
+++ src/PracticeDay03/Fibonacci.java
@@ -0,0 +1,25 @@
+package PracticeDay03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+
+    public static List<Integer> fibonacci(int number)
+    {
+        List<Integer> list = new ArrayList<>();
+        if(number == 0) return list;
+        list.add(0);
+        if(number == 1) return list;
+        list.add(1);
+        for(int i = 2; i < number; i ++)
+        {
+            list.add(list.get(i-1)+list.get(i-2));
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fibonacci.fibonacci(44));
+    }
+}
