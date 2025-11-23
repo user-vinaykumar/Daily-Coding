9db0869ab68cbf9bc1ae4bbb3ee6cdaafd123ce4
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-23 19:26:33 +0530

    program to check whether the given number or string is palindrome or not.

---
+++ src/PracticeDay04/Palindrome.java
@@ -0,0 +1,16 @@
+package PracticeDay04;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String word)
+    {
+        String term = String.valueOf(word);
+        return term.equals(new StringBuilder(term).reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("virtusa"));
+        System.out.println(Palindrome.isPalindrome("malayalam"));
+
+    }
+}
