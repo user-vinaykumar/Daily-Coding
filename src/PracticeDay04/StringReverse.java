333b87f4a3e30b4eb92f1bfab42600a241b1777b
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-23 19:47:23 +0530

    reverse the string withou using the stringbuilder function.

---
+++ src/PracticeDay04/StringReverse.java
@@ -0,0 +1,24 @@
+package PracticeDay04;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+public class StringReverse {
+
+    public static String stringReverse(String word)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : word.toCharArray())
+        {
+            stack.push(c);
+        }
+        String result = stack.stream().map(String::valueOf).collect(Collectors.joining());
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("virtusa"));
+    }
+}
