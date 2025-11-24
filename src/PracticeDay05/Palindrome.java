20b41505e6612f41de826b307e09cc624d332536
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-24 19:12:28 +0530

    program to check palindrome string.

---
+++ src/PracticeDay05/Palindrome.java
@@ -0,0 +1,15 @@
+package PracticeDay05;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String word)
+    {
+        String term = String.valueOf(word);
+        return term.equals(new StringBuilder(term).reverse().toString());
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("virtusa"));
+    }
+}
