2f9e4beb418015ed7543a2dd1368c5fca437f369
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-24 20:07:09 +0530

    program to check the prime number.

---
+++ src/PracticeDay05/PrimeNumber.java
@@ -0,0 +1,17 @@
+package PracticeDay05;
+
+import java.math.BigInteger;
+
+public class PrimeNumber {
+
+    public static boolean isPrimeNumber(int number)
+    {
+        return BigInteger.valueOf(number).isProbablePrime(number);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PrimeNumber.isPrimeNumber(9));
+        System.out.println(PrimeNumber.isPrimeNumber(12));
+        System.out.println(PrimeNumber.isPrimeNumber(13));
+    }
+}
