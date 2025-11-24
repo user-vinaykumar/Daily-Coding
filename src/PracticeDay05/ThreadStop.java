476a4b4c082a5e2a4dc7978e0ceab652eaf94f36
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-24 19:48:15 +0530

    program to stop the runnable thread implementation.

---
+++ src/PracticeDay05/ThreadStop.java
@@ -0,0 +1,50 @@
+package PracticeDay05;
+
+public class ThreadStop implements Runnable {
+
+    private boolean flag = false;
+
+    public void turnoff()
+    {
+        this.flag = true;
+    }
+
+    public boolean turnedoff()
+    {
+        return this.flag;
+    }
+
+    public void sleep(long milis)
+    {
+        try {
+            Thread.sleep(milis);
+        }catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void run()
+    {
+        System.out.println("started");
+        while (!turnedoff())
+        {
+            sleep(1000);
+            System.out.println("---");
+        }
+        System.out.println("stopped");
+    }
+
+
+    public static void main(String[] args) {
+        ThreadStop threadStop = new ThreadStop();
+        Thread thread = new Thread(threadStop);
+        thread.start();
+
+        threadStop.sleep(5000);
+        System.out.println("turn off");
+        threadStop.turnoff();
+        System.out.println("turned off");
+    }
+
+}
