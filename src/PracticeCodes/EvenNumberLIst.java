02a4df81a6fdfc1b641e9d89770fbbaf95b3761f
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-25 15:53:50 +0530

    program to check for the even numbers in a list of numbers.

--- src/PracticeCodes/EvenNumberLIst.java
+++ src/PracticeCodes/EvenNumberLIst.java
@@ -1,4 +1,19 @@
 package PracticeCodes;
 
+import PracticeDay05.EvenNumberList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EvenNumberLIst {
+
+    public static List<Integer> evenNumberList(List<Integer> list)
+    {
+        return list.stream().filter(n->n%2==0).toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        System.out.println(EvenNumberList.evenNumberList(inputList));
+    }
 }
