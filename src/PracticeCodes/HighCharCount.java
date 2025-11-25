2d260b4b6c3176e451303f61172491040f0c263b
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-25 15:25:10 +0530

    program to check the character in a word that repeated most number of times.

---
+++ src/PracticeCodes/HighCharCount.java
@@ -0,0 +1,29 @@
+package PracticeCodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighCharCount {
+
+    public static void highCharCount(String word)
+    {
+        int count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : word.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        Map.Entry<Character, Integer> maxEntry = map.entrySet().stream()
+                .max(Map.Entry.comparingByValue()).orElse(null);
+        if(maxEntry!=null)
+        {
+            System.out.println(maxEntry.getKey()+":"+maxEntry.getValue());
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        HighCharCount.highCharCount("virrtusa");
+
+    }
+}
