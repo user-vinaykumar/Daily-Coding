f41e9ccb809dc0a6eddbb1a3dd8d25d07530084a
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-26 13:02:41 +0530

    two sum dsa program.

---
+++ src/RoughWork/TwoSum.java
@@ -0,0 +1,28 @@
+package RoughWork;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[]nums, int target)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++)
+        {
+            int difference = target - nums[i];
+            if(map.containsKey(difference))
+            {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {2, 3,4, 5, 6, 7, 8, 9};
+        int[]result = TwoSum.twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+    }
+}
