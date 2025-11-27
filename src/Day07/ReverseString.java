93b5f04723e066d74613eeadbabca4c824c898e8
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-27 16:08:11 +0530

    String Reverse program without using StringBuilder function.

---
+++ src/Day07/ReverseString.java
@@ -0,0 +1,24 @@
+package Day07;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+public class ReverseString {
+
+    public static String reverseString(String word)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : word.toCharArray())
+        {
+            stack.push(c);
+        }
+        String result = stack.stream().map(String::valueOf).collect(Collectors.joining());
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(ReverseString.reverseString("virtusa"));
+    }
+}
