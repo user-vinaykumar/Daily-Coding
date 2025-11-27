80e0f678c68801a92bda687cff6194b57ea55b5c
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-27 15:18:16 +0530

    Even number list in the list of numbers.

---
+++ src/Day07/EvenNumberList.java
@@ -0,0 +1,18 @@
+package Day07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenNumberList {
+
+    public static List<Integer> evenNumberList(List<Integer> list)
+    {
+        return list.stream().filter(n->n%2==0).toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 8, 9, 0, 33));
+        System.out.println(EvenNumberList.evenNumberList(inputList));
+    }
+}
