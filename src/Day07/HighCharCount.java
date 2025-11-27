b51334f6132d61729b1a2e6b7588cea76c9bda8f
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-27 15:30:04 +0530

    Characters repeated most number of times in the given String.

---
+++ src/Day07/HighCharCount.java
@@ -0,0 +1,26 @@
+package Day07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighCharCount {
+
+    public static void highCharCount(String word)
+    {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : word.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        Map.Entry<Character, Integer> maxEntry = map.entrySet()
+                .stream().max(Map.Entry.comparingByValue()).orElse(null);
+        if(maxEntry!=null)
+        {
+            System.out.println(maxEntry.getKey()+":"+maxEntry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        HighCharCount.highCharCount("malayalam");
+    }
+}
