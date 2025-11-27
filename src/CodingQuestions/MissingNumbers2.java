33b696a26565752978f73545ab1a3274f90be1d2
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-27 16:37:50 +0530

    Missing Numbers program with Stream topics.

---
+++ src/CodingQuestions/MissingNumbers2.java
@@ -0,0 +1,26 @@
+package CodingQuestions;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MissingNumbers2 {
+
+    public static List<Integer> missingNumber2(List<Integer> list) {
+        if (list == null || list.isEmpty()) return Collections.emptyList();
+
+        List<Integer> outputList = new ArrayList<>();
+        Set<Integer> set = new HashSet<>(list);
+
+        int min = Collections.min(set);
+        int max = Collections.max(set);
+
+        return IntStream.range(min, max).filter(n -> !set.contains(n))
+                .boxed().collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 2, 3, 9));
+        System.out.println(MissingNumbers2.missingNumber2(inputList));
+    }
+}
