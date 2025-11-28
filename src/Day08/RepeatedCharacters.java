d2ffe893b8daf8526db3dcbd962312c31105c0e3
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-28 15:45:23 +0530

    program to check the characters in a string whether they are repeated more than once or not.

---
+++ src/Day08/RepeatedCharacters.java
@@ -0,0 +1,30 @@
+package Day08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepeatedCharacters {
+
+    public static void repeatedCharacters(String word)
+    {
+        int count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : word.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        for(Map.Entry<Character, Integer> maxEntry : map.entrySet())
+        {
+            if(maxEntry.getValue()>1)
+            {
+                System.out.println(maxEntry.getKey()+":"+maxEntry.getValue());
+                count++;
+            }
+        }
+        if(count==0) System.out.println("No character has repeated more than once.");
+    }
+
+    public static void main(String[] args) {
+        RepeatedCharacters.repeatedCharacters("virrtusa");
+    }
+}
