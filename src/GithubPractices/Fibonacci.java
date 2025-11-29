6b21585dee5156f585ca58b5e9d65bdf1811f505
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-29 14:22:14 +0530

    Fibonacci series program.

---
+++ src/GithubPractices/Fibonacci.java
@@ -0,0 +1,25 @@
+package GithubPractices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+
+    public static List<Integer> fibonacci(int number)
+    {
+        List<Integer> outputList = new ArrayList<>();
+        if(number == 0) return outputList;
+        outputList.add(0);
+        if(number == 1) return outputList;
+        outputList.add(1);
+        for(int i = 2; i < number; i ++)
+        {
+            outputList.add(outputList.get(i-1)+outputList.get(i-2));
+        }
+        return outputList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fibonacci.fibonacci(18));
+    }
+}
