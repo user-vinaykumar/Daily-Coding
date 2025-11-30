91ad7839dfa72e23fccb52ac59ea4fe86507be26
user_vk_ <rossilorenzo81773@gmail.com>
2025-11-30 13:01:59 +0530

    word character count in the given string with streams logic

---
+++ src/CodingQuestions/WordCharCountStream.java
@@ -0,0 +1,23 @@
+package CodingQuestions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class WordCharCountStream {
+
+    public static void wordCharCount(String word)
+    {
+        LinkedHashMap<Character, Integer> map = word.chars()
+                .mapToObj(c->(char) c)
+                .collect(Collectors.groupingBy(c->c, LinkedHashMap::new, Collectors.summingInt(x->1)));
+        for(Map.Entry<Character, Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        WordCharCountStream.wordCharCount("virtusa");
+    }
+}
