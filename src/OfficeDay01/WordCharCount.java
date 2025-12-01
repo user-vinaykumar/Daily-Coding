1e8c42bb164129361517163e9cab95d849a8dd04
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-01 13:46:17 +0530

    character count in a given string.

---
+++ src/OfficeDay01/WordCharCount.java
@@ -0,0 +1,22 @@
+package OfficeDay01;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class WordCharCount {
+
+    public static void wordCharCount(String word)
+    {
+        LinkedHashMap<Character, Integer> map = word.chars().mapToObj(c-> (char) c)
+                .collect(Collectors.groupingBy(c->c, LinkedHashMap::new, Collectors.summingInt(x->1)));
+        for(Map.Entry<Character, Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        WordCharCount.wordCharCount("virtusa");
+    }
+}
