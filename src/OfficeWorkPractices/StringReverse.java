428b82939aef13b8fbf747f8eb4aa24bce8a2658
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-02 16:21:23 +0530

    string reverse with stringbuilder function.

---
+++ src/OfficeWorkPractices/StringReverse.java
@@ -0,0 +1,13 @@
+package OfficeWorkPractices;
+
+public class StringReverse {
+
+    public static String stringReverse(String word)
+    {
+        return new StringBuilder(word).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("virtusa"));
+    }
+}
