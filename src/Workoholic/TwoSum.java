fd315ba81dcf265b460cf7b264a9e961afc24a31
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-03 15:19:30 +0530

    Two Sum DSA program.

---
+++ src/Workoholic/TwoSum.java
@@ -0,0 +1,29 @@
+package Workoholic;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[]nums, int target)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++)
+        {
+            int difference = target - nums[i];
+            if(map.containsKey(difference))
+            {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {1, 2, 3, 4, 5, 6};
+        int[]res = TwoSum.twoSum(nums, 11);
+        System.out.println(Arrays.toString(res));
+    }
+}
