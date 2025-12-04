a9d6ec32979d2b059420625e7a804395e03b02bd
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-04 15:34:40 +0530

    String Reverse without string Builder function.

---
+++ src/GitPushes/StringReverse.java
@@ -0,0 +1,23 @@
+package GitPushes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+public class StringReverse {
+
+    public static String stringReverse(String word)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : word.toCharArray())
+        {
+            stack.push(c);
+        }
+        String str = stack.stream().map(String::valueOf).collect(Collectors.joining());
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("virtusa"));
+    }
+}
