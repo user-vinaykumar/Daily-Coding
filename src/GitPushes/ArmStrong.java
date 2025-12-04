7d858fde7012338355cd443cf24447b85495e743
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-04 15:51:03 +0530

    Armstrong Number Program.

---
+++ src/GitPushes/ArmStrong.java
@@ -0,0 +1,29 @@
+package GitPushes;
+
+import GithubPractices.Armstron;
+import Workoholic.Armstrong;
+
+import java.math.BigInteger;
+
+public class ArmStrong {
+
+    public static boolean armstrong(BigInteger number)
+    {
+        String str = String.valueOf(number);
+        int exponential = str.length();
+        BigInteger sum = BigInteger.ZERO;
+        for(char c : str.toCharArray())
+        {
+            int digit = c - '0';
+            BigInteger bigInteger = BigInteger.valueOf(digit);
+            sum = sum.add(bigInteger.pow(exponential));
+        }
+        return sum.equals(number);
+    }
+
+    public static void main(String[] args) {
+        BigInteger input = BigInteger.valueOf(153);
+        if(ArmStrong.armstrong(input)) System.out.println("the provided number is an armstrong number");
+        else System.out.println("the provided number is not an armstrong number");
+    }
+}
