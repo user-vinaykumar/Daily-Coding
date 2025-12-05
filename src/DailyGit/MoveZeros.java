0d44f6890cc6bd9fa142c30bc04a9680829d8c13
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-05 13:37:10 +0530

    Move Zeros to the end of the List.

---
+++ src/DailyGit/MoveZeros.java
@@ -0,0 +1,27 @@
+package DailyGit;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MoveZeros {
+
+    public static List<Integer> moveZerosToTheLast(List<Integer> list)
+    {
+        List<Integer> zeroList = list
+                .stream()
+                .filter(n-> n ==0)
+                .toList();
+        List<Integer> nonZeroList = list
+                .stream()
+                .filter(n-> n!=0)
+                        .collect(Collectors.toList());
+
+        nonZeroList.addAll(zeroList);
+        return nonZeroList;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1,2,0,0,0,4,4,3,4,0,0,4,5,0,8);
+        System.out.println(MoveZeros.moveZerosToTheLast(inputList));
+    }
+}
