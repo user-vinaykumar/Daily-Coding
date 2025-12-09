7930b11c3582dd383a8ff272589ae7666051a266
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-09 12:44:13 +0530

    highly repeated character in the given string.

---
+++ src/GitCodePushDay/HighCharacterCount.java
@@ -0,0 +1,32 @@
+package GitCodePushDay;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HighCharacterCount {
+
+    public static void highCharCount(String word)
+    {
+        Map<Character, Integer> map = word
+                .chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(c->c, HashMap::new,
+                        Collectors.summingInt(x->1)));
+
+        Map.Entry<Character, Integer> entries = map.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .orElse(null);
+
+        if(entries!=null)
+        {
+            System.out.println(entries.getKey()+":"+entries.getValue());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        HighCharacterCount.highCharCount("virtttttttusa");
+    }
+}
