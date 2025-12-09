7b27b4ac5ba4c66e9072ab9c67b9ea8cbbbdc249
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-09 12:10:23 +0530

    String Reverse

---
+++ src/GitCodePushDay/StringReverse.java
@@ -0,0 +1,12 @@
+package GitCodePushDay;
+
+public class StringReverse {
+
+    public static String stringReverse(String word) {
+        return new StringBuilder(word).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("virtusa"));
+    }
+}
