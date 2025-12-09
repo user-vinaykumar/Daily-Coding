a9e7476fdb1f02c510bce550f1476d24118d711a
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-09 12:33:50 +0530

    Highest repeated Character in the String.

---
+++ src/GitCodePushDay/HighCharCount.java
@@ -0,0 +1,35 @@
+package GitCodePushDay;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HighCharCount {
+
+    public static void highCharCount(String word)
+    {
+        int count = 0;
+        Map<Character, Integer> map = word
+                .chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(c->c, HashMap::new,
+                        Collectors.summingInt(x->1)));
+
+        for(Map.Entry<Character, Integer> entries : map.entrySet())
+        {
+            if(entries.getValue()>1)
+            {
+                System.out.println(entries.getKey()+":"+entries.getValue());
+                count++;
+            }
+        }
+        if(count==0)
+        {
+            System.out.println("No characters in the string has repeated more than once.");
+        }
+    }
+
+    public static void main(String[] args) {
+     HighCharCount.highCharCount("virttusa");
+    }
+}
