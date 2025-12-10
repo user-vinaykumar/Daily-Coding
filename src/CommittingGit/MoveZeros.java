c631dd443bd1223c51aa16e5cec28163373e7439
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-10 15:42:43 +0530

    move zeros to the end of the list.

---
+++ src/CommittingGit/MoveZeros.java
@@ -0,0 +1,28 @@
+package CommittingGit;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MoveZeros {
+
+    public static List<Integer> moveZeros(List<Integer> list)
+    {
+        List<Integer> zeroList = list
+                .stream()
+                .filter(n-> n == 0)
+                .toList();
+
+        List<Integer> nonZeroList = list
+                .stream()
+                .filter(n-> n != 0)
+                .collect(Collectors.toList());
+
+        nonZeroList.addAll(zeroList);
+        return nonZeroList;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 0, 3, 0, 4, 0, 0, 0, 3);
+        System.out.println(MoveZeros.moveZeros(inputList));
+    }
+}
