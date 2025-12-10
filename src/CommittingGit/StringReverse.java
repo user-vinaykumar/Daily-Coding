6ebbb33a8373debd5e2fe55f2480d3cfa9aac346
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-10 12:55:17 +0530

    StringReverse with String builder

---
+++ src/CommittingGit/StringReverse.java
@@ -0,0 +1,14 @@
+package CommittingGit;
+
+public class StringReverse {
+
+    public static String stringReverse(String word)
+    {
+        return new StringBuilder(word).reverse().toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("virtusa"));
+    }
+}
