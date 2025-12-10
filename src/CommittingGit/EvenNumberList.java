b05ef6769dc306260589437c1f1766c42ffe51ff
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-10 14:42:10 +0530

    Even Number List

---
+++ src/CommittingGit/EvenNumberList.java
@@ -0,0 +1,15 @@
+package CommittingGit;
+
+import java.util.List;
+
+public class EvenNumberList {
+
+    public static List<Integer> evenNumber(List<Integer> list) {
+        return list.stream().filter(n -> n % 2 == 0).toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 0, 11, 13, 17, 22, 33, 21);
+        System.out.println(EvenNumberList.evenNumber(inputList));
+    }
+}
