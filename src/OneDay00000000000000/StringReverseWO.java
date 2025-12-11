97b978590b9fe1cf0c72887d046c375fe47014e1
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-11 12:10:32 +0530

    String Reverse without StringBuilder.

---
+++ src/OneDay00000000000000/StringReverseWO.java
@@ -0,0 +1,26 @@
+package OneDay00000000000000;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+public class StringReverseWO {
+
+    public static String stringReverseWO(String word) {
+        Deque<Character> stack = word
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(ArrayDeque::new, Deque::push, Deque::addAll);
+
+        String str = stack
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringReverseWO.stringReverseWO("virtusa"));
+    }
+}
