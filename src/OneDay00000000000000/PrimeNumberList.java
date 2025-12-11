f5aee438927b0da448649bc372fea71044d1248d
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-11 12:58:50 +0530

    Prime Number List.

---
+++ src/OneDay00000000000000/PrimeNumberList.java
@@ -0,0 +1,19 @@
+package OneDay00000000000000;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class PrimeNumberList {
+
+    public static List<Integer> primeList(List<Integer> list) {
+        return list
+                .stream()
+                .filter(n -> BigInteger.valueOf(n).isProbablePrime(n))
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 3, 4, 53, 2, 6, 7, 8, 9, 13, 17);
+        System.out.println(PrimeNumberList.primeList(inputList));
+    }
+}
