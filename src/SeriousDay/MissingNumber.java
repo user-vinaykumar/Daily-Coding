95e9eb2d25688c9b906769cf758643f5210fcb90
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-15 12:36:16 +0530

    Missing Number in the List program.

---
+++ src/SeriousDay/MissingNumber.java
@@ -0,0 +1,29 @@
+package SeriousDay;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class MissingNumber {
+
+    public static List<Integer> missingNumber(List<Integer> list) {
+        List<Integer> uniqueSortedList = list
+                .stream()
+                .distinct()
+                .sorted()
+                .toList();
+
+        int min = Collections.min(uniqueSortedList);
+        int max = Collections.max(uniqueSortedList);
+
+        return IntStream.range(min, max)
+                .filter(n -> !uniqueSortedList.contains(n))
+                .boxed()
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 8);
+        System.out.println(MissingNumber.missingNumber(inputList));
+    }
+}
