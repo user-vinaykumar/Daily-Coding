90f8fc58fe23f023a096a3b7548f85ae879715ae
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-15 12:16:09 +0530

    HighChar Count program.

---
+++ src/SeriousDay/HighCharCount.java
@@ -0,0 +1,28 @@
+package SeriousDay;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HighCharCount {
+
+    public static void highCharCount(String word) {
+        Map<Character, Integer> map = word
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, LinkedHashMap::new,
+                        Collectors.summingInt(x -> 1)));
+
+        Map.Entry<Character, Integer> entries = map.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .orElse(null);
+        if (entries != null) {
+            System.out.println(entries.getKey() + ":" + entries.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        HighCharCount.highCharCount("virrtusa");
+    }
+}
