9b6504a1d081965e2dabe1ad2c46d427415efea7
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-15 12:09:28 +0530

    String Reverse without String Builder.

---
+++ src/SeriousDay/WordReverse.java
@@ -0,0 +1,25 @@
+package SeriousDay;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+public class WordReverse {
+
+    public static String wordReverse(String word) {
+        Deque<Character> stack = word
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(ArrayDeque::new, Deque::push, Deque::addAll);
+
+        String str = stack
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(WordReverse.wordReverse("virtusa"));
+    }
+}
