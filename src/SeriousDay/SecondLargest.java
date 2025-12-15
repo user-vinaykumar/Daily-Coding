de72a37bdbf3112e0396d22795719806a82de715
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-15 12:48:29 +0530

    Second Largest Number in a List.

---
+++ src/SeriousDay/SecondLargest.java
@@ -0,0 +1,21 @@
+package SeriousDay;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SecondLargest {
+
+    public static int secondLargestNumber(List<Integer> list) {
+        return list
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(0000000000000000);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        System.out.println(SecondLargest.secondLargestNumber(inputList));
+    }
+}
