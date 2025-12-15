3a30f2840f3858b463c257226205f0e2ad4cf1b5
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-15 12:27:43 +0530

    EvenNumber List

---
+++ src/SeriousDay/EvenNumberList.java
@@ -0,0 +1,18 @@
+package SeriousDay;
+
+import java.util.List;
+
+public class EvenNumberList {
+
+    public static List<Integer> evenList(List<Integer> list) {
+        return list
+                .stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        System.out.println(EvenNumberList.evenList(inputList));
+    }
+}
