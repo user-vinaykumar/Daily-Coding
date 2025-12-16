cb616841f2585f74315b239594ae8ffb7c5671bc
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-16 15:20:13 +0530

    String Reverse and Entire code base of the daily push.

---
+++ src/TuesdayPush/TwoSum.java
@@ -0,0 +1,28 @@
+package TuesdayPush;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[]nums, int target)
+    {
+        HashMap<Integer, Integer>map = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++)
+        {
+            int difference = target - nums[i];
+            if(map.containsKey(difference))
+            {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] res = TwoSum.twoSum(nums, 13);
+        System.out.println(Arrays.toString(res));
+    }
+}
