64de0faf42910c9878eea8de7d23db17aa142575
user_vk_ <rossilorenzo81773@gmail.com>
2025-12-16 15:40:09 +0530

    WordCharCount program.

---
+++ src/DecemberPush/WordCharCount.java
@@ -0,0 +1,24 @@
+package DecemberPush;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class WordCharCount {
+
+    public static void wordCharCount(String word) {
+        Map<Character, Integer> map = word
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, LinkedHashMap::new,
+                        Collectors.summingInt(x -> 1)));
+
+        for (Map.Entry<Character, Integer> entries : map.entrySet()) {
+            System.out.println(entries.getKey() + ":" + entries.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        WordCharCount.wordCharCount("virtusa");
+    }
+}
